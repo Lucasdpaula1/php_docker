9f2600c5e71cae727d645fd94fd8b80226cefe47
lucas <lucassidpaula@gmail.com>
2025-02-27 20:06:25 -0300

    corrigindo o erro de sintaxe

--- python/app/app.py
+++ python/app/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def send_email():
   data = request.json
   intialization_smtp_server(message=data['context'],subject=data['subject'],recevier_email=data['to'])
-  return jsonify(f'<h1> envio de email para o {data.get('to')}</h1>')
+  return jsonify(f'<h1> envio de email para o {data['to']}</h1>')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000,debug=True)
